987f0c76352bf811a09d5d50e4fb6e09ae956c44
newtonrodrigues <newton.nw100@gmail.com>
2022-01-09 13:33:58 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace Aula
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // POO
+            Veiculo veiculo = new Veiculo();
+
+            veiculo.Cor = "Vermelho";
+            Console.WriteLine("Marca do veículo: " + veiculo.Marca + " " + veiculo.Cor);
+            Console.WriteLine("A cor da roda é: " + veiculo.CorDaRoda);
+
+            Console.WriteLine("---------------------------------------------------------");
+
+            Veiculo veiculo2 = new Veiculo();
+            veiculo2.Cor = "Amarelo";
+            veiculo2.Marca = "Fiat";
+            veiculo2.CorDaRoda = "Roxo";
+            Console.WriteLine("A cor do veículo 2 é: " + veiculo2.Cor + " " + veiculo2.Marca);
+            Console.WriteLine("O veículo tem roda? " + veiculo2.TemRoda);
+            Console.WriteLine("A cor da roda é: " + veiculo2.CorDaRoda);
+
+            Console.WriteLine("------------------------------------------------------------");
+
+            Veiculo veiculo3 = new Veiculo();
+            veiculo3.setCilindrada("150 cc");
+            veiculo3.Acelerar();
+            
+            Console.WriteLine(veiculo3.getCilindrada());
+
+            //veiculo3.Consumo();
+            Console.WriteLine(veiculo3.Consumo());
+
+            veiculo3.setConsumoPorLitro(5);
+            Console.WriteLine("O veículo 3 anda " + veiculo3.getConsumoPorLitro() + " quilometros por litro.");
+
+            Console.WriteLine("---------------------------------------------------------------");
+            Console.WriteLine("Quantos quilometros pretende viajar? ");
+            string valorDigitado = Console.ReadLine();
+            double valor = Convert.ToDouble(valorDigitado);
+
+            Console.WriteLine("Quantos quilometros seu carro faz por litro? ");
+            string valorPorLitro = Console.ReadLine();
+            double valorConvertidoPorLitro = Convert.ToDouble(valorPorLitro);
+
+           // veiculo3.resultadoQuilometragem(valor, valorConvertidoPorLitro);
+
+            Console.WriteLine("Você precisará de: " + veiculo3.resultadoQuilometragem(valor, valorConvertidoPorLitro) + " litros para realizar a viagem.");
+
+        }
+    }
+}
---
+++ Veiculo.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aula
+{
+    class Veiculo
+    {
+        public string Cor;
+        public string Modelo = "201";
+        private string Cilindrada;
+        public string Marca = "Volkswagen";
+        public bool TemRoda = true;
+
+        public void setCilindrada(string Cilindrada)
+        {
+            this.Cilindrada = Cilindrada;
+        }
+
+        public string getCilindrada()
+        {
+            return Cilindrada;
+        }
+
+        public string CorDaRoda = "Preto";
+
+        public void Acelerar()
+        {
+            Console.WriteLine("Acelerando");
+        }
+
+        //public void Veiculo( string Cor, string Cilindrada )
+        //    {
+        //        this.Cilindrada = "150 cc";
+        //        this.Cor = "Azul";
+        //  Método construtor, sempre será void e tem o mesmo nome da classe que irá instanciar. 
+        //    }
+
+      public Veiculo()
+        {
+            Console.WriteLine("Classe veículo sendo construída: ");
+        }
+
+        public int Consumo()
+        {
+            int consumo = 10;
+            return consumo;
+        }
+
+        private int ConsumoPorLitro;
+
+        public void setConsumoPorLitro(int ConsumoPorLitro)
+        {
+            this.ConsumoPorLitro = ConsumoPorLitro;
+        }
+
+        public int getConsumoPorLitro()
+        {
+            return ConsumoPorLitro;
+        }
+
+        public double resultadoQuilometragem(double valor1, double valorConvertidoPorLitro2)
+        {
+            double resultado = (valor1 / valorConvertidoPorLitro2);
+            return resultado;
+        }
+
+    }
+}
